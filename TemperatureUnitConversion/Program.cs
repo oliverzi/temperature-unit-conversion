1186db7aa8c4470674fba46b182e0a210c02fa83
oliverzi <oliver.zivcic@gmail.com>
2022-07-04 21:30:23 +0200

    Add project files.

---
+++ TemperatureUnitConversion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TempUnitConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 0;
+            Console.Write("Enter the number you want to convert: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the unit of the number which you want to convert (C,F,K): ");
+            string ua = Console.ReadLine();
+            Console.Write("Enter the desired unit (C,F,K): ");
+            string ub = Console.ReadLine();
+
+        }
+    }
+}
